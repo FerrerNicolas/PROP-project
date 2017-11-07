fe5dc1d11679fd2f4affa844c955b65825586ee3
FerrerNicolas <guillem.ferrer.nicolas@gmail.com>
2017-11-07 19:46:56 +0100

    Completed Board.java Subject to changes, as always

--- src/model/Board.java
+++ src/model/Board.java
@@ -10,20 +10,61 @@ public class Board {
 	public Board() {
 	}
 	
+	//
+	//getters
+	//
+	
 	public Code getSecretCode() {
 		return secretCode;
 	}
+	
+	public ArrayList<Code> getGuesses(){
+		return guesses;
+	}
+	
+	public ArrayList<Correction> getCorrections(){
+		return corrections;
+	}
+	
+	//
+	//setters
+	//
 	
 	public void setSecretCode(Code secretC) {
 		secretCode = secretC;
 	}
 	
+	//
+	//others
+	//
+	
+	//assumes the code is valid
 	public void addGuess(Code guess){
 		guesses.add(guess);
 	}
 	
+	//returns true if 12 turns have passed, false otherwise
+	//assumes the correction is valid
 	public Boolean addCorrection(Correction corr) {
 		corrections.add(corr);
+		if (corrections.size() == 12)
+			return true;
+		return false;
 	}
-	//= new ArrayList<Code>();
+	
+	//checks, using the last correction, if the game has been won
+	public Boolean hasWon() {
+		Correction lastCorrection = corrections.get( corrections.size() - 1 );
+		if (lastCorrection.getBlackPins() == 4)
+			return true;
+		return false;
+	}
+	
+	//returns the number of corrections
+	//assumes the number of turns is equal to the number of turns done
+	public int turnsDone() {
+		return corrections.size();
+	}
+	
+	
 }
