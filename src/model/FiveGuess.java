49ea806b5620fe1306802a105ca983dffc5c78ce
KayandTheBlack <victor.gimenez.abalos@gmail.com>
2017-11-08 16:50:26 +0100

    Finished FiveGuess Algorithm, will probably need to come back to debug.

--- src/model/FiveGuess.java
+++ src/model/FiveGuess.java
@@ -28,14 +28,50 @@ public class FiveGuess extends Ai {
 	}
 	
 	public Code codeBreakerTurn(Code code, Correction correction) { //In case javi does not have it, consulting board may be best (through game)
-		if(code == null && correction == null) return new Code(1122);
-		//Exception handling! 1 null or whatevs
-		
-		// eliminate from S!
-		for (int i=0; i<S.size(); i++) {
-			
+		if(code == null && correction == null) {
+			return new Code(1122);
 		}
-		return new Code();
+		//Exception handling! 1 null or whatevs
+
+		unusedGuesses.remove(code);
+		// eliminate from S
+		int i=0;
+		while(i<S.size()) {
+			if(!S.get(i).isCompatible(code, correction)) S.remove(i);
+			else i++;
+		}
+		
+		//...
+		Code nextGuess = new Code(); //Change to code.
+		int minScore = 0;
+		for(i = 0; i < unusedGuesses.size(); i++) {
+			ArrayList<ArrayList<Integer> > hitCount =  new ArrayList<ArrayList<Integer> >();
+			for(int a=0; a < 5; a++) {
+				ArrayList<Integer> r = new ArrayList<Integer>();
+				for(int b = 0; b < 5; b++) r.add(0);
+				hitCount.add(r);
+			}
+			for(int j=0; j < S.size(); j++) {
+				Correction c = S.get(j).correct(unusedGuesses.get(i));
+				Integer aux = hitCount.get(c.getBlackPins()).get(c.getWhitePins());
+				hitCount.get(c.getBlackPins()).set(c.getWhitePins(), aux+1);
+			}
+			int localMaxHitCount=0;
+			for(int a=0; a<5; a++) {
+				for(int b=0; b<5; b++) {
+					localMaxHitCount = Math.max(localMaxHitCount, hitCount.get(a).get(b));
+				}
+			}
+			int score = S.size()-localMaxHitCount;
+			if (score > minScore) {
+				minScore = score;
+				nextGuess = unusedGuesses.get(i).clone();
+			} else if(score == minScore && S.contains( unusedGuesses.get(i) ) ) {
+				nextGuess = unusedGuesses.get(i).clone();
+				// We do this to ensure picking elements preferably from S
+			}
+		}
+		return nextGuess;
 	}
 	
 }
