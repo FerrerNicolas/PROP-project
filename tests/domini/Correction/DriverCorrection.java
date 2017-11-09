f30b7bdfe7e3bff5f8a2ee2889cb28dd48ff2d64
FerrerNicolas <guillem.ferrer.nicolas@gmail.com>
2017-11-09 20:29:30 +0100

    Finished DriverCorrection.java

--- tests/domini/Correction/DriverCorrection.java
+++ tests/domini/Correction/DriverCorrection.java
@@ -5,8 +5,16 @@ import java.util.*;
 public class DriverCorrection {
 	private static Correction correction;
 	
-	public void test1() {
-		
+	public static void test1(Scanner sc) {
+    	correction = new Correction(0,0);
+    	System.out.println("Input number of white pins for the correction to create: ");
+    	int w = sc.nextInt();
+    	correction.setWhitePins(w);
+    	System.out.println("Input number of black pins for the correction to create: ");
+    	int b = sc.nextInt();
+    	correction.setBlackPins(b);
+
+    	System.out.println("Created correction with " + w + " white pins and " + b + " black pins");
 	}
 	public static void test2() {
 		if (correction == null) {
@@ -17,7 +25,7 @@ public class DriverCorrection {
     		System.out.println(p);
     	}
 	}
-	public void test3() {
+	public static void test3() {
 		if (correction == null) {
     		System.out.println("Invalid Operation, you need to create a correction first using Option 1");
     	}else {
@@ -26,11 +34,11 @@ public class DriverCorrection {
     		System.out.println(p);
     	}
 	}
-	public void test4(Scanner sc) {
+	public static void test4(Scanner sc) {
 		if (correction == null) {
     		System.out.println("Invalid Operation, you need to create a correction first using Option 1");
     	}else {
-    		System.out.print("Input number of pins: ");
+    		System.out.println("Input number of pins: ");
     		int q = sc.nextInt();
     		correction.setWhitePins(q);
     		int p = correction.getWhitePins();
@@ -38,11 +46,11 @@ public class DriverCorrection {
     		System.out.println(p);
     	}
 	}
-	public void test5(Scanner sc) {
+	public static void test5(Scanner sc) {
 		if (correction == null) {
     		System.out.println("Invalid Operation, you need to create a correction first using Option 1");
     	}else {
-    		System.out.print("Input number of pins: ");
+    		System.out.println("Input number of pins: ");
     		int q = sc.nextInt();
     		correction.setBlackPins(q);
     		int p = correction.getBlackPins();
@@ -50,29 +58,28 @@ public class DriverCorrection {
     		System.out.println(p);
     	}
 	}
-	public void test6(Scanner sc) {
+	public static void test6(Scanner sc) {
 		if (correction == null) {
     		System.out.println("Invalid Operation, you need to create a correction first using Option 1");
     	}else {
     		Correction tmp = new Correction(0,0);
-    		System.out.print("Input number of white pins for the correction to compare to: ");
+    		System.out.println("Input number of white pins for the correction to compare to: ");
     		int w = sc.nextInt();
     		tmp.setWhitePins(w);
-    		System.out.print("Input number of black pins for the correction to compare to: ");
+    		System.out.println("Input number of black pins for the correction to compare to: ");
     		int b = sc.nextInt();
     		tmp.setBlackPins(b);
     		
     		int p = correction.getBlackPins();
     		int q = correction.getWhitePins();
     		
-    		boolean isEqual = correction.equals(tmp)
-    		System.out.println("Compared to " + q + " white pins and " + p + " black pins, got ");
+    		boolean isEqual = correction.equals(tmp);
+    		System.out.println("Compared to " + q + " white pins and " + p + " black pins, got " + isEqual);
     	}
 	}
 	
-	
-	public static void main(String[] args) {
-        System.out.println("Choose what to test:");
+	public static void printMenu() {
+		System.out.println("Choose what to test:");
         System.out.println("1- Create a correction (needed for tests 2 through 6).");
         System.out.println("2- Get number of white pins");
         System.out.println("3- Get number of black pins");
@@ -80,31 +87,43 @@ public class DriverCorrection {
         System.out.println("5- Set number of black pins");
         System.out.println("6- Check wheter the correction equals another (meaning having equal attributes)");
         System.out.println("7- End test");
+	}
+	
+	public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         boolean theresMore = true;
         while (theresMore) {
+        	
+        	printMenu();
+        	
             int option = sc.nextInt();
             sc.nextLine();
             switch (option) {
                 case 1:
-                   
+                    test1(sc);
                     break;
                 case 2:
                 	test2();
                     break;
                 case 3:
-                    
+                    test3();
                     break;
                 case 4:
-                    
+                    test4(sc);
+                    break;
+                case 5:
+                    test5(sc);
+                    break;
+                case 6:
+                    test6(sc);
                     break;
                 case 7:
                 	theresMore = false;
                     break;
                 default:
-                    System.out.println("Invalid Option.");
+                    System.out.println("Invalid Option, try again.");
             }
         }
     }
