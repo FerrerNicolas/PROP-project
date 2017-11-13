ba221bac57c44746afa7470721445c76d45a13d9
Luis Mendes <luismendes535@gmail.com>
2017-11-13 18:59:50 +0000

    domini/Ranking

--- tests/domini/Ranking/Ranking.java
+++ tests/domini/Ranking/Ranking.java
@@ -17,7 +17,7 @@ public class Ranking { //Author:Luis
 		sort(ranking);
 	}
 		
-		/*Function to sort array using insertion sort*/
+		/*Function to sort arrayList using insertion sort*/
 	    void sort(ArrayList<Tuple> ranking)
 	    {
 	        int n = ranking.size();
@@ -37,33 +37,8 @@ public class Ranking { //Author:Luis
 	            ranking.set(j+1,key);
 	        }
 	    }
-	    
-/*
-				if(ranking.size()<10) { //Fill ArrayList with first 10 tuples
-				ranking.add(t);
-				
-			}
-			else { 
-
-			for(int i=0;i < ranking.size();i++) { 
-					if(ranking.get(i).getValue() < t.getValue() ) { //Check if values inside the array are lower than the new tuple value
-						if(ranking.get(i).getPlayerName().equals(t.getPlayerName())) { //One player must have only one register in the ranking list
-							ranking.set(i, t); //Replace tuple 
-							
-
-							}
-					else{
-							ranking.add(t); //Add tuple with score greater than the old tuple
-
-						}
-			}
-					else {
-						
-					}
-		}
-			}*/
-				
-	public ArrayList<Tuple> getRanking(){
+	  
+	    public ArrayList<Tuple> getRanking(){
 		return ranking;
 				}
 	}
