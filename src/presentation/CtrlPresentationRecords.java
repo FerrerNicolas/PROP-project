9564ea460242370c09f33aceb0d58385f633ebf7
KayandTheBlack <victor.gimenez.abalos@gmail.com>
2018-01-01 20:09:12 +0100

    Tried uploading Luis' stuff

---
+++ src/presentation/CtrlPresentationRecords.java
@@ -0,0 +1,42 @@
+package presentation;
+
+import domini.Tuple.Tuple;
+import model.Player;
+import domain.CtrlDomain;
+import domain.CtrlDomainRecords;
+
+
+public class CtrlPresentationRecords {
+
+    protected CtrlDomainRecords cdr;
+
+    protected RecordsGUI recordsGui;
+
+    private CtrlDomain domain;
+
+    public CtrlPresentationRecords() {
+        try {
+            this.domain = new CtrlDomain();
+
+        } catch (Exception e) {
+        
+            e.printStackTrace();
+        
+        }
+        // Plyer name
+        String playername = this.domain.getLoggedUsername();
+        
+        Player player =  (Player)this.domain.getInstanceOfPlayer(playername);
+        // max Score for the rankings
+        
+        Tuple playerScore = new Tuple(player.getPlayerName(), player.getMaxScore());
+        
+        //
+        Tuple playerR
+        
+        this.recordsGui = new RecordsGUI(this.domain.getInstanceOfPlayer(playername), domain);
+
+    }
+        
+    
+}
